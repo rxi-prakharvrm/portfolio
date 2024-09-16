544be61db9d893224c0879278d0440abcde2caa4
Prakhar Verma <prakharverma1022@gmail.com>
2024-09-16 15:56:30 +0530

    update about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,15 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className="content-section">
+    <div className="content-section about-section">
+      <div className="portfolio-img-ctr">
+        <img src="images/prakhar-flip.png" alt="Prakhar Verma" />
+      </div>
       <p class="portfolio-desc">
-        I am a passionate front-end web developer with a strong command of HTML, CSS, JavaScript, and React.js. I specialize in creating visually appealing, responsive, and user-friendly interfaces that enhance user experience. My projects, including 'The Spicy Meal' and the 'Code Auction' platform, reflect my ability to turn design concepts into fully functional websites with smooth performance.  
-        <br />  
-        <br />  
-        I excel in modern front-end technologies like React, utilizing state management, event handling, and dynamic styling to build seamless web applications. My expertise in tools like TailwindCSS, AOS, and Figma allows me to design and implement pixel-perfect layouts while ensuring a clean and efficient codebase. I enjoy exploring new front-end libraries and frameworks to stay ahead in the fast-evolving web development landscape.  
-        <br />  
-        <br />  
-        With a strong eye for detail and a focus on performance optimization, I aim to create engaging digital experiences that align with user needs and business goals. My approach emphasizes collaboration, using Git for version control and best practices for code management, ensuring every project I work on is well-organized and scalable.
+        I am a passionate front-end web developer with a strong command of <strong>HTML</strong>, <strong>CSS</strong>, <strong>JavaScript</strong>, and <strong>React.js</strong>. I specialize in creating visually appealing, responsive, and user-friendly interfaces that enhance user experience.
       </p>
     </div>
   )
